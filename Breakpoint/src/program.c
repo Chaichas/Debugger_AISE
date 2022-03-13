3aa7be9e7a2b82ecf00fdebee7c3ca188f5c595a
aicha <aichamaaoui1>
2022-03-13 18:44:37 +0100

    Add Program.c
    In this commit, I added the program.c in which I defined the hello function in a loop.

---
+++ Breakpoint/src/program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void print_hello(){
+    printf("Hello");
+}
+
+
+//Main program
+int main(){
+
+    for (int i = 0; i < 2; ++i)
+        print_hello(); 
+        
+    //printf("CHPS\n");
+    
+    return 0;
+}
