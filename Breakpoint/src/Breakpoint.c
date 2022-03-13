7832c2c950241b045242bac722832a6735025e4d
aicha <aichamaaoui1>
2022-03-13 13:53:06 +0100

    Check child process pointer in Breakpoint.c
    In this commit, I added the Check child process pointer in Breakpoint.c.

--- Breakpoint/src/Breakpoint.c
+++ Breakpoint/src/Breakpoint.c
@@ -7,9 +7,19 @@ void debugger_breakpoint()
 	..
 }
 
-void debugger_run()
+void debugger_run(pid_t pid)
 {
-	..
+	//Starting of the debugger program
+	procmsg("*** Debugger Breakpoint *** \n");	
+	procmsg("Start: \n");
+	
+	//waiting for child to change its state
+	procmsg("Waiting for child process to change its state \n");
+	wait(0);
+	
+	//Wait is over and child changed its state: Check its position (pointer)
+	procmsg("child now at pointer position = %p \n", get_child_eip(pid));
+	
 }
 
 int main(int argc, char* argv)
