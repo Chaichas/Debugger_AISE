77d85c544bb399a4d36d564141c91ac875b780fa
aicha <aichamaaoui1>
2022-03-15 20:20:12 +0100

    Add attribute in real time of processes
    In this commit, I added the real time attribute using chrt.

--- scr/build.sh
+++ scr/build.sh
@@ -2,16 +2,19 @@
 #Reference: https://www.baeldung.com/linux/show-shared-libraries-executables
 
 #objdump: displayes information from object files. It is combined with grep to list the required shared libraries of a program.
-
 objdump -p File | grep 'NEEDED'
 
 
 #Display the values of environment variables
 #Reference: https://www.cyberciti.biz/faq/linux-list-all-environment-variables-env-command/
- 
 printenv
 
 
 #Display all available functions
 #Reference: https://stackoverflow.com/questions/4471364/how-do-i-list-the-functions-defined-in-my-shell
 set | grep " ()"
+
+
+#chrt:  contrôle de processus avancés qui permet de déterminer et d’ajuster les attributs en temps réel 
+#Reference: https://www.ionos.fr/digitalguide/serveur/configuration/commandes-linux/?
+chrt -p PID
