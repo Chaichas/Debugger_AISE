9a4e42abe60ec290603d1f911f112629c16ae612
aicha <aichamaaoui1>
2022-03-15 20:06:47 +0100

    Add display the values of environment variables in build.sh
    In this commit, I added the display the values of environment variables in the build.sh file

--- scr/build.sh
+++ scr/build.sh
@@ -4,3 +4,9 @@
 #objdump: displayes information from object files. It is combined with grep to list the required shared libraries of a program.
 
 objdump -p File | grep 'NEEDED'
+
+
+#display the values of environment variables
+#Reference: https://www.cyberciti.biz/faq/linux-list-all-environment-variables-env-command/
+ 
+printenv
