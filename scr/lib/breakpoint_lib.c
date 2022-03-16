facba2002370b7da03a0ddebaace259749e61ff0
aicha <aichamaaoui1>
2022-03-16 19:19:32 +0100

    Add breakpoint_true function in breakpoint_lib.c
    In this commit, I added the breakpoint_true function in the breakpoint_lib.c.

--- scr/lib/breakpoint_lib.c
+++ scr/lib/breakpoint_lib.c
@@ -1,1 +1,9 @@
+ #include "breakpoint_lib.h"
  
+//Insert trap instruction at the corresponding address + save original data in it
+void breakpoint_true(pid_t pid, debug_breakpoint* breakp){
+
+	assert(breakp);
+	breakp->Data = ptrace(PTRACE_PEEKTEXT, pid, breakp->address, 0);
+	ptrace(PTRACE_POKETEXT, pid, breakp->address, (breakp->Data & 0xFFFFFF00) | 0xCC);
+}
--- scr/lib/breakpoint_lib.h
+++ scr/lib/breakpoint_lib.h
@@ -1,1 +1,12 @@
+ #ifndef BREAKPOINT_DEBUGGER
+ #define BREAKPOINT_DEBUGGER
  
+typedef struct debug_breakpoint_type debug_breakpoint;
+struct debug_breakpoint_type {
+	void* address;
+	unsigned Data;
+}; 
+
+void breakpoint_true(pid_t pid, debug_breakpoint* breakp);
+
+#endif
