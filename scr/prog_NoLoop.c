797a9eef70b0458e8c46a78c5bc714963156237f
aicha <aichamaaoui1>
2022-03-16 11:46:12 +0100

    Add prog_NoLoop.c file
    In this commit, I added the prog_NoLoop.c file, used to test the breakpoint program.

---
+++ scr/prog_NoLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Function say_hello, which we will create before a breakpoint in the debugger
+void say_hello(){
+
+	printf("Hello and welcome in the CHPS Master M1 !"); }
+
+//Main program
+int main(){
+	say_hello();
+	return 0;
+}
