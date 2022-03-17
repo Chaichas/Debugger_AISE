9d4c604eda9db94e77c478dac74b5e5f57fbc911
aicha <aichamaaoui1>
2022-03-17 18:58:52 +0100

    Update breakpoint_lib
    In this commit, I updated the breakpoint_lib.h and .c.

--- scr/lib/breakpoint_lib.h
+++ scr/lib/breakpoint_lib.h
@@ -1,10 +1,25 @@
  #ifndef BREAKPOINT_DEBUGGER
  #define BREAKPOINT_DEBUGGER
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <syscall.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <errno.h>
+#include <assert.h>
  
 typedef struct debug_breakpoint_type debug_breakpoint;
 struct debug_breakpoint_type {
 	void* address;
-	unsigned Data;
+	unsigned data;
 }; 
 
 void breakpoint_true(pid_t pid, debug_breakpoint* breakp);
