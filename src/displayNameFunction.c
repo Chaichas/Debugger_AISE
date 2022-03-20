7e2fb46b1f4e0897ceb7e32b6d54ee7b895e6900
aicha <aichamaaoui1>
2022-03-20 12:48:47 +0100

    Update displayNameFunction.c
    In this commit, I updated displayNameFunction.c.

--- src/displayNameFunction.c
+++ src/displayNameFunction.c
@@ -4,41 +4,80 @@
 #include <stdio.h>
 #include <string.h>
  
-void check(char *c,int p1, int p2);
-void display(char *c, int p1);
- 
-void main(int argc, char **argv)
+// Print name of function character by character
+void display_function_name(char *c,int p1)
 {
-    FILE *fp;
-    char ch[100];
-    char *pos1, *pos2, *pos3;
+    int temp = p1, i;
  
-    fp=fopen(argv[1], "r");
-    if (fp == NULL)
+    while (c[--temp] != ' ');
+    for (i = temp + 1; i < p1; i++){ 
+        printf("%c", c[i]);}
+	printf("\n");
+    return;
+ 
+}
+
+//Check if it is a function
+void check_function(char *c, int p1, int p2)
+{
+    int i, flag = 0, temp = p1;
+ 
+    if ((c[p1 + 1] == ')'))
     {
-        printf("\nFile unable to open");
+        display_function_name(c, p1);
         return;
     }
-    else
-        printf("\n");
-	printf("*********************** Functions in the Program: *********************** \n");
-	printf("\n");
-    while (1)
+    for (i = p1 + 1; i < p2; i++)
     {
-        if ((fgets(ch, 100, fp)) != NULL)
+        if ((c[i] != ' ') || (c[i] == ')'))
+        {
+            flag = 1;
+ 
+        }
+        if (flag == 0)
+        {
+            display_function_name(c, p1);
+            return;
+        }
+        else
+        {
+            flag = 0;
+            while (c[--temp] != ' ');
+            for (i = 0; i < temp; i++)
+                if (c[i]==' ')
+                {
+                    flag = 1;
+                }
+                if (flag == 0)
+                {
+                    display_function_name(c, p1);
+                    return;
+                 }
+                 else
+                     return;
+          }
+    }
+}
+
+//Check the functions in a program 
+void function_prog (FILE *fp, char *ch, char *position1, char *position2, char *position3){
+while (1)
+    {
+        if ((fgets(ch, 100, fp)) != NULL) //read char of a file and store it in a tab
         {
             if ((strstr(ch, "/*")) == NULL)
             {
-                pos1 = strchr(ch, '(');                /* check opening brace */
-                if (pos1)
+                position1 = strchr(ch, '('); //check if there is opening brace (
+                if (position1)
                 {
-                    pos2 = strchr(ch,')');            /* check oclosing brace */
-                    if (pos2)
+                    position2 = strchr(ch,')'); //chech if there is closing brace)
+                    if (position2)
                     {
-                        pos3 = strchr(ch,';');        /* check for semicolon */
-                        if ((pos1 < pos2) && (pos3 == NULL) || (pos3 < pos1))
+                        position3 = strchr(ch,';'); //check if there is a semi-colomn;
+                        
+                        if ((position1 < position2) && (position3 == NULL) || (position3 < position1))
                         {
-                            check(ch, pos1 - ch, pos2 - ch);
+                            check_function(ch, position1 - ch, position2 - ch);
                         }
                         else    continue;
                     }
@@ -53,57 +92,24 @@ void main(int argc, char **argv)
     fclose(fp);
 }
  
-/* To check if it is a function */
-void check(char *c, int p1, int p2)
+
+//Main Program 
+void main(int argc, char **argv)
 {
-    int i, flag = 0, temp = p1;
+    FILE *fp;
+    char ch[100];
+    char *position1, *position2, *position3;
  
-    if ((c[p1 + 1] == ')'))
+    fp=fopen(argv[1], "r"); //open file
+    if (fp == NULL)
     {
-        display(c, p1);
+        printf("\nFile unable to open");
         return;
     }
-    for (i = p1 + 1; i < p2; i++)
-    {
-        if ((c[i] != ' ') || (c[i] == ')'))
-        {
-            flag = 1;
- 
-        }
-        if (flag == 0)
-        {
-            display(c, p1);
-            return;
-        }
-        else
-        {
-            flag = 0;
-            while (c[--temp] != ' ');
-            for (i = 0; i < temp; i++)
-                if (c[i]==' ')
-                {
-                    flag = 1;
-                }
-                if (flag == 0)
-                {
-                    display(c, p1);
-                    return;
-                 }
-                 else
-                     return;
-          }
-    }
-}
- 
-/* To display function name */
-void display(char *c,int p1)
-{
-    int temp = p1, i;
- 
-    while (c[--temp] != ' ');
-    for (i = temp + 1; i < p1; i++)            /* Print name of function character by character */
-        printf("%c", c[i]);
+    else
+        printf("\n");
+	printf("*********************** Functions in the Program: *********************** \n");
+	function_prog (fp, ch, position1, position2, position3);
 	printf("\n");
-    return;
- 
+    
 }
